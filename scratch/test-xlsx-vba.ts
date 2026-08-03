220f2d7dfee6bea19be24a98af684fe3ae1b039e
SmartClinic Admin <microsoftportharcourt@gmail.com>
2026-08-03 16:29:31 +0100

    fix: remove scratch directory to fix build error

--- scratch/test-xlsx-vba.ts
+++
@@ -1,6 +0,0 @@
-import * as XLSX from 'xlsx';
-
-// create a dummy workbook with macros? Hard to do on the fly.
-// I will just check if TypeScript allows workbook.vbaraw
-const wb = XLSX.utils.book_new();
-console.log('vbaraw exists?', 'vbaraw' in wb);
